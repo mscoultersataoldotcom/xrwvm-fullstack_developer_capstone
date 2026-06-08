8e84a0fde437a17acfafe9956b476de030d25da2
mscoultersataoldotcom <139816368+mscoultersataoldotcom@users.noreply.github.com>
2026-06-08 10:51:51 -0400

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,13 +29,13 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                 'https://mscoulters-8000.theianext-0-labs'\
+                 'https://mscoulters-8000.theianext-0-labs'
                  '-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-                 'https://mscoulters-8000.theiadockernext-1-labs'\
+                 'https://mscoulters-8000.theiadockernext-1-labs'
                  '-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://mscoulters-8000.theianext-0-labs'\
+CSRF_TRUSTED_ORIGINS = ['https://mscoulters-8000.theianext-0-labs'
                         '-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-                        'https://mscoulters-8000.theiadockernext-1-labs'\
+                        'https://mscoulters-8000.theiadockernext-1-labs'
                         '-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
@@ -101,7 +101,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
